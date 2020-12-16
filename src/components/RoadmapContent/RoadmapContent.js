22f555784563002ea7409b9974944b9dab920422
jshsandberg <jshsandberg@gmail.com>
2020-12-16 14:01:46 -0800

    hello

--- src/components/RoadmapContent/RoadmapContent.js
+++ src/components/RoadmapContent/RoadmapContent.js
@@ -29,8 +29,7 @@ function RoadmapContent() {
                 <div  className="container">
                     <div className="row">
                         <div style={{backgroundColor: "white", border: "solid"}} className="col-5">
-                        <p>0It is safe to assume that by fitting you mean covering all of the width. This is becauseYou typically do not know the height just by using col-sm-6 or col-md-6 or col-lg-4.There is a huge probability of loss in aspect ratio of the image if you try to resize it according to your own will.Use img src = "images/dummy_image.png" class = "img-responsive" width = "100%" for fitting the column. This will fit your image width-wise into the c</p>
-
+                            <p>Using HTML / CSS / Javascript / Jquery / Ajax on the Front End and Node.js / MySQL / MySQL Workbench / Heroku / Sequelize / dotENV / Express / HandleBars / NodeMailer  on the Back End, our team was able to create a functional and well-polished roadtrip tracking application. Calling it Road Trip Planner, our application allows the user to click on any state and save, delete, and view other users notes. Each state contains facts about them and are displayed when the corresponding state is clicked on. We wanted to create an application that targeted travellers and aventure seekers and help them keep track of their experinces. </p>
                         </div>
                         <div className="col-7">
                             <img style={{maxWidth: "100%"}} src={Picture3} />
@@ -42,15 +41,15 @@ function RoadmapContent() {
                             <img style={{maxWidth: "100%"}} src={Picture1} />
                         </div>
                         <div style={{backgroundColor: "white", border: "solid"}} className="col-4">
-                        <p>0It is safe to assume that by fitting you mean covering all of the width. This is becauseYou typically do not know the height just by using col-sm-6 or col-md-6 or col-lg-4.There is a huge probability of loss in aspect ratio of the image if you try to resize it according to your own will.Use img src = "images/dummy_image.png" class = "img-responsive" width = "100%" for fitting the column. This will fit your image width-wise into the c</p>
-
+                            <p>On the Front End, I had to draw out each individual state on the US map and give them their own ID and data to track which state the user interacted with using Inkscape. In the fact.js file, I had to learn and master ES6 in order to interact with dynamic data. I use Ajax to communicate with the backend on each individual note the user creates.</p>
                         </div>
                     </div>
                     <br></br>
                     <div className="row">
                         <div style={{backgroundColor: "white", border: "solid"}} className="col-5">
-                        <p>0It is safe to assume that by fitting you mean covering all of the width. This is becauseYou typically do not know the height just by using col-sm-6 or col-md-6 or col-lg-4.There is a huge probability of loss in aspect ratio of the image if you try to resize it according to your own will.Use img src = "images/dummy_image.png" class = "img-responsive" width = "100%" for fitting the column. This will fit your image width-wise into the c</p>
-
+                            <p>With the Back End, we used MySQL and Sequelize as our ORM. Being our first time with this implementing a Full Stack Applicaiton, the Back End cause a lot of issues and hardships. We found that using Sequelize on such a small application was not practical; however, once we got the ORM working, it opened a lot of possibilites that we wanted to implement in our application. This application allows for reads, creates, and deletes of the user notes.</p>
+                            <br></br>
+                            <h3>Checkout our the application: <a href="https://roadtrip-planner-app.herokuapp.com/">RoadTrip Planner</a></h3>
                         </div>
                         <div className="col-7">
                             <img style={{maxWidth: "100%"}} src={Picture2} />   
